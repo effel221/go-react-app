d4c63032a4e10e31047fbb91688829f36a878908
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-05-27 23:22:44 +0200

    add fetch mock to test

--- frontend/src/Controls/Controls.test.tsx
+++ frontend/src/Controls/Controls.test.tsx
@@ -1,8 +1,13 @@
 import { render, fireEvent } from '@testing-library/react'
 import * as useSelectorOutput from "../lib/hooks";
-import {describe, it, vi, afterEach, beforeEach, expect} from 'vitest';
+import * as fetchResult from "../lib/useDataFetch";
+import {describe, it, vi, afterEach, beforeEach, expect, Mock} from 'vitest';
 import StoreProvider from "../StoreProvider";
 import Controls from "./index";
+import {CardInterface} from "../lib/interfaces";
+
+
+global.fetch = vi.fn() as Mock;
 
 describe('Controls', () => {
     const mockCard = [{
@@ -11,6 +16,9 @@ describe('Controls', () => {
         asset: 'test asset',
         selected: true
     }]
+    function createFetchResponse(data:CardInterface[]) {
+        return { json: () => new Promise((resolve) => resolve(data)) }
+    }
     beforeEach(() => {
       vi.clearAllMocks();
     });
@@ -20,6 +28,9 @@ describe('Controls', () => {
     it('renders the Controls and trigger redux store update on button click', () => {
         const spy = vi.spyOn(useSelectorOutput, 'useAppSelector');
         spy.mockImplementationOnce(() => mockCard);
+        const useDataFetch = vi.spyOn(fetchResult, 'useDataFetch');
+        useDataFetch.mockImplementationOnce(() => mockCard);
+        (fetch as Mock).mockResolvedValue(createFetchResponse(mockCard))
         const { container } = render(<StoreProvider><Controls/></StoreProvider>);
         const button = container.querySelector('.controls-sorting-desc') as HTMLButtonElement;
         fireEvent.click(button);
